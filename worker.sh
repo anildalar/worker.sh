74fcf77a31c5fbf9e8130aac6ef3faa9a08e8411
ANIL DOLLOR <dollorinfotech@gmail.com>
2025-01-09 18:14:42 +0530

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -10,7 +10,7 @@ sudo systemctl restart containerd
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
-sudo apt install -y kubelet kubeadm kubectl
+sudo apt install -y kubelet kubeadm kubectl --allow-change-held-packages
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
